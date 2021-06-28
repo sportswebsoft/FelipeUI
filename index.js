a0042362b8423ad572e472f1e7def6f68e321f6b
Felipe Suarez <suarez.a.felipe@gmail.com>
2021-06-28 10:42:08 -0500

    Add top section of the navbar. Add logo

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-$(document).on('click', '.yamm .dropdown-menu', function(e) {
-  e.stopPropagation()
-})
+$(document).on("click", ".yamm .dropdown-menu", function (e) {
+  e.stopPropagation();
+});
+
+$(function () {
+  const updateMainNavbar = function () {
+    if ($(window).width() < 992) {
+      $("#ul-main-menu").removeClass("flex-row");
+      $(".mobile-nav-link").addClass("nav-link");
+    } else {
+      $("#ul-main-menu").addClass("flex-row");
+      $(".mobile-nav-link").removeClass("nav-link");
+    }
+  };
+
+  // Call on first load
+  updateMainNavbar();
+
+  // Call again on every window resize
+  $(window).resize(function () {
+    updateMainNavbar();
+  });
+});
