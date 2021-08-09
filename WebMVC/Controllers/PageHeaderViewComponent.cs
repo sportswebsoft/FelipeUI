02ffa0a3e7c36ba56ff84322910a41497ab93489
Felipe Suarez <suarez.a.felipe@gmail.com>
2021-08-09 01:23:05 -0500

    Add PageHeaderViewComponent and display the top menu dynamically from that model

---
+++ WebMVC/Controllers/PageHeaderViewComponent.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class PageHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // simulates async behaviour (for ex. fetching from DB)
+            PageHeaderModel model = await Task.Run(MockPageHeaderModel);
+            return View("_PageHeaderPartial", model);
+        }
+        
+        private static PageHeaderModel MockPageHeaderModel()
+        {
+            MenuItem football = new()
+            {
+                DisplayName = "Football",
+                Url = "#football"
+            };
+            
+            MenuItem girlsVolleyball = new()
+            {
+                DisplayName = "Girls Volleyball",
+                Url = "#girls-volleyball"
+            };
+            
+            MenuItem boysSoccer = new()
+            {
+                DisplayName = "Boys Soccer",
+                Url = "#boys-soccer"
+            };
+
+            var menuItems = new List<MenuItem>
+            {
+                football,
+                girlsVolleyball,
+                boysSoccer
+            };
+
+            MenuHeader menuHeader = new()
+            {
+                DisplayTitle = "Fall Sports",
+                MenuItems = menuItems
+            };
+
+            var menuHeaderList = new List<MenuHeader>()
+            {
+                menuHeader,
+                menuHeader,
+                menuHeader
+            };
+            
+            return new PageHeaderModel
+            {
+                ConferenceName = "Greater Miami Conference from VIEW COMPONENT",
+                LogoFileName = "gmc-logo.png",
+                ColorCode = "5490af",
+                MenuHeaders = menuHeaderList
+            };
+        }
+    }
+}
---
+++ WebMVC/Models/PageHeaderModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace WebMVC.Models
+{
+    public class PageHeaderModel
+    {
+        public string ConferenceName { get; set; }
+        public string LogoFileName { get; set; }
+        public string ColorCode { get; set; }
+        public IList<MenuHeader> MenuHeaders { get; set; }
+    }
+
+    public class MenuHeader
+    {
+        public string DisplayTitle { get; set; }
+        public IList<MenuItem> MenuItems { get; set; }
+    }
+
+    public class MenuItem
+    {
+        public string DisplayName { get; set; }
+        public string Url { get; set; }
+    }
+}
