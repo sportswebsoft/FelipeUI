e0ff872ed39587441e9071f7368b8d668f95df5f
Felipe Suarez <suarez.a.felipe@gmail.com>
2021-08-09 22:06:11 -0500

    Add json menu file for mvc and ggcl conferences

---
+++ WebMVC/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace WebMVC.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string SanitizeForUrl(this string input)
+        {
+            var sanitizedStr = input
+                .ToLowerInvariant()
+                .Replace(" ", "-")
+                .Replace("'", "-");
+            
+            return sanitizedStr;
+        }
+    }
+}
--- WebMVC/Models/PageHeaderModel.cs
+++ WebMVC/Models/PageHeaderModel.cs
@@ -1,24 +1,27 @@
 ﻿using System.Collections.Generic;
+using WebMVC.Extensions;
 
 namespace WebMVC.Models
 {
     public class PageHeaderModel
     {
         public string ConferenceName { get; set; }
+
+        public string ConferenceNameUrl => ConferenceName.SanitizeForUrl(); 
+        
         public string LogoFileName { get; set; }
+        
         public string ColorCode { get; set; }
+        
         public IList<MenuHeader> MenuHeaders { get; set; }
     }
 
     public class MenuHeader
     {
         public string DisplayTitle { get; set; }
-        public IList<MenuItem> MenuItems { get; set; }
-    }
-
-    public class MenuItem
-    {
-        public string DisplayName { get; set; }
-        public string Url { get; set; }
+        
+        public string DisplayTitleUrl => DisplayTitle.SanitizeForUrl();
+        
+        public IList<string> MenuItems { get; set; }
     }
 }
